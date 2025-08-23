ce9d6f0bf556337908caa750c7d968c86dada4f9
agorapilled <johngardner13@outlook.com>
2025-08-22 20:24:49 -0400

    v0

--- src/features/constants/elevenLabsParam.ts
+++ src/features/constants/elevenLabsParam.ts
@@ -3,5 +3,5 @@ export type ElevenLabsParam = {
 };
 
 export const DEFAULT_ELEVEN_LABS_PARAM: ElevenLabsParam = {
-    voiceId: "479awuEQRWC4PxB3tfUB", // Elli
+    voiceId: "CwhRBWXzGAHq8TQ4Fs17", // Elli
 } as const;
